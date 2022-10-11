54f229dde5197ca28248b8407448fb0697539286
Swetha Sundar <42051002+swetha-sundar@users.noreply.github.com>
2022-10-11 18:22:13 +0800

    Move to console log

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,9 +1,11 @@
 import Link from 'next/link'
 
 export default function IndexPage() {
+  console.log(process.env);
+  console.log(process.env.SECRET_NAME_VAR);
   return (
     <div>
-      Hello World.{process.env.SECRET_NAME_VAR}
+      Hello World.
       <Link href="/about">
         <a>About</a>
       </Link>
